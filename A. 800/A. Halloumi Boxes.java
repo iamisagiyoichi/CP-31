23f00dd51a94a5e877449d3807f839b821cdac51
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-11 23:21:30 +0530

    Add Main class for Halloumi Boxes problem solution

---
+++ A. 800/A. Halloumi Boxes.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static boolean isSorted(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            if(arr[i-1] > arr[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            
+            int arr[] = new int[n];
+            for(int i=0;i<n;i++){
+                arr[i] = sc.nextInt();
+            }
+            if(k > 1){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println(isSorted(arr) ? "YES" : "NO");
+            }
+        }
+        sc.close();
+    }
+}
